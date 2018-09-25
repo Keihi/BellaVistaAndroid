7660430d040b9a10d8a892d11b6cc621d354e283
keihi <carlos.a.d.s.f@hotmail.com>
2018-09-25 12:01:32 -0300

    alguna coisa

--- app/src/main/java/com/bellavista/bellavistaandroid/AgendaActivity.java
+++ app/src/main/java/com/bellavista/bellavistaandroid/AgendaActivity.java
@@ -2,6 +2,7 @@ package com.bellavista.bellavistaandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class AgendaActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class AgendaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agenda);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
